396d91bd2145d17718e8cffa3fbc989fc2516c99
heimsharon <heimsharon@hotmail.com>
2025-09-05 00:36:57 -0400

    index.css and homepage updates

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,4 +1,13 @@
-import React, { useEffect } from 'react';
+import React { useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import '../assets/styles/homepage.css'
+import '..assets/styles/homepage.css'
 
+const Home: React.FC = () => {
+  return (
+    <HomeContainer>
+    <Title>Welcome to the Knitted Shop</Title>
+    <p> Find your knitted item to cherish</p>
+    </HomeContainer>
+  )
+}
+}
