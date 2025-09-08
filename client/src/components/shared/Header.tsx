c9ec575d896b5bafaedfb1abc9f59e32838e8bfe
heimsharon <heimsharon@hotmail.com>
2025-09-08 17:17:26 -0400

    added missing ) and }

--- client/src/components/shared/Header.tsx
+++ client/src/components/shared/Header.tsx
@@ -2,46 +2,45 @@ import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import '//' //update style page link
 
-const Header:React.FC = () => {
+const Header: React.FC = () => {
   const location = useLocation();
   const navigate = useNavigate();
 
-  const hideUserActionsOn = [ '/login', 'signup'];
+  const hideUserActionsOn = ['/login', 'signup'];
   const isAuthenticated = !!localStorage.getItem('token');
 
   return (
     <header className='header'>
       <div
-      className='header-logo-group'
-      onClick={() => navigate ('/')
+        className='header-logo-group'
+        onClick={() => navigate('/')}
         aria-label="Crocheted Mushroom"
         role='img'
-        />
-        {!hideUserActionsOn.includes(location.pathname) && (
-          <div className='user-actions'>
+      />
+      {!hideUserActionsOn.includes(location.pathname) && (
+        <div className='user-actions'>
           {!isAuthenticated ? (
             <button
-            className='login-btn'
-            onClick={() => navigate('/login')}
+              className='login-btn'
+              onClick={() => navigate('/login')}
             >
               Login
             </button>
-            </div>
-        ): (
-          <button
-          className='logout-btn'
-          onClick={() =>{
-            localStorage.removeItem('token');
-            navigate('/');
-          }}
-          >
-            Log Out
-          </button>
-        )}
-      }
-  );
-      <h1> The Crocheted Mushroom</h1>
+
+          ) : (
+            <button
+              className='logout-btn'
+              onClick={() => {
+                localStorage.removeItem('token');
+                navigate('/');
+              }}
+            >
+              LogOut
+            </button>
+          )}
+        </div>
+      )}
     </header>
-  
+  );
 };
 export default Header;
