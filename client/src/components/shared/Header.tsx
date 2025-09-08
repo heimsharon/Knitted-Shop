4364bd20caa25e9a46dfc36624b9aaa113bd5a57
heimsharon <heimsharon@hotmail.com>
2025-09-08 17:09:15 -0400

    added content to header file

---
+++ client/src/components/shared/Header.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import '//' //update style page link
+
+const Header:React.FC = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const hideUserActionsOn = [ '/login', 'signup'];
+  const isAuthenticated = !!localStorage.getItem('token');
+
+  return (
+    <header className='header'>
+      <div
+      className='header-logo-group'
+      onClick={() => navigate ('/')
+        aria-label="Crocheted Mushroom"
+        role='img'
+        />
+        {!hideUserActionsOn.includes(location.pathname) && (
+          <div className='user-actions'>
+          {!isAuthenticated ? (
+            <button
+            className='login-btn'
+            onClick={() => navigate('/login')}
+            >
+              Login
+            </button>
+            </div>
+        ): (
+          <button
+          className='logout-btn'
+          onClick={() =>{
+            localStorage.removeItem('token');
+            navigate('/');
+          }}
+          >
+            Log Out
+          </button>
+        )}
+      }
+  );
+      <h1> The Crocheted Mushroom</h1>
+    </header>
+  
+};
+export default Header;
